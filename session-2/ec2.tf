2be629cacd9d6579bf5f34627ef50ac20083a015
Bektursun <bektursunkudaiberen@gmail.com>
2025-05-05 17:03:17 -0500

    session-2-classwork

--- session-2/ec2.tf
+++ session-2/ec2.tf
@@ -1,39 +1,39 @@
-# resource "aws_instance" "first_ec2" {
-#   ami           = "ami-07b0c09aab6e66ee9"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name        = "testing"
-#     Environment = "dev"
-#   }
-# }
+resource "aws_instance" "first_ec2" {
+  ami           = "ami-07b0c09aab6e66ee9"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "testing"
+    Environment = "dev"
+  }
+}
 
-# resource "aws_instance" "second_ec2" {
-#   ami           = "ami-07b0c09aab6e66ee9"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name        = "test1"
-#     Environment = "dev"
-#   }
-# }
+resource "aws_instance" "second_ec2" {
+  ami           = "ami-07b0c09aab6e66ee9"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "test1"
+    Environment = "dev"
+  }
+}
 
-# resource "aws_security_group" "simple_sg" {
-#   name = "simple-sg"
-#   description = "This is a test sg"
+resource "aws_security_group" "simple_sg" {
+  name = "simple-sg"
+  description = "This is a test sg"
 
-# ingress {
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
+ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   
-# egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-# }
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
 
 
 
