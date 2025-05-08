a60f275f81bac883870304ca22cc2e182bf9d296
Bektursun <bektursunkudaiberen@gmail.com>
2025-05-08 17:49:38 -0500

    minor

--- session-3-hw-count/route_tables.tf
+++ session-3-hw-count/route_tables.tf
@@ -10,7 +10,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 
-resource "aws_route_table" "private_route_table " {
+resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
   route { 
     cidr_block = "0.0.0.0/0"
