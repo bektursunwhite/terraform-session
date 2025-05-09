027015e4f1205ef27531091be66ccb8294d8ae30
Bektursun <bektursunkudaiberen@gmail.com>
2025-05-09 15:36:35 -0500

    added

--- session-3-hw-count/association.tf
+++ session-3-hw-count/association.tf
@@ -1,11 +1,11 @@
 resource "aws_route_table_association" "public_subnet_associate" {
- count = length(var.public_subnets_cidrs)
- subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ count = length(aws_subnet.public_subnets)
+ subnet_id      = aws_subnet.public_subnets[count.index].id
  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "private_subnet_associate" {
-  count = length(var.private_subnets_cidrs)
-  subnet_id = element(aws_subnet.private_subnets[*].id, count.index)
+  count = length(aws_subnet.private_subnets)
+  subnet_id = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.private_route_table.id
 }
--- session-3-hw-count/nat_gateway.tf
+++ session-3-hw-count/nat_gateway.tf
@@ -6,7 +6,7 @@ resource "aws_eip" "elastic_ip" {
 
 resource "aws_nat_gateway" "nat_gw" {
    allocation_id = aws_eip.elastic_ip.id
-   subnet_id = aws_subnet.private_subnets.id
+   subnet_id = aws_subnet.public_subnets[0].id
    tags = { 
     Name = "NAT-gw"
    }
